021c049a1cd88983c42ba039276f15aefbe6fdc9
Eduar-SC <https://github.com/Eduar-SC>
2025-06-27 15:27:14 -0500

    otra vezzzzz

--- frontend/src/pages/ConsultarCartera.jsx
+++ frontend/src/pages/ConsultarCartera.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/consultarCartera.css";
+import "../styles/consultarcartera.css";
 
 export default function ConsultarCartera() {
     const [tipo, setTipo] = useState("");
